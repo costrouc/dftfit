c135a1d38247fbf52bfc4b83fc0ea87f48a16c9d
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-03-16 17:44:14 -0400

    Partially working write potentials from evaluation

--- dftfit/cli/db.py
+++ dftfit/cli/db.py
@@ -3,13 +3,14 @@ import argparse
 
 from .utils import is_file_type
 from ..db import DatabaseManager
-from ..db_actions import copy_database_to_database
+from ..db_actions import copy_database_to_database, select_potential_from_evaluation
 
 
 def add_subcommand_db(subparsers):
     parser = subparsers.add_parser('db', help='database management')
     sub_subparsers = parser.add_subparsers()
     add_subcommand_db_merge(sub_subparsers)
+    add_subcommand_db_potential(sub_subparsers)
 
 
 def add_subcommand_db_merge(subparsers):
@@ -19,6 +20,14 @@ def add_subcommand_db_merge(subparsers):
     parser.add_argument('-o', '--output-database', help='output database', required=True)
     parser.add_argument('-u', '--unique', dest='unique', action='store_true', help='only insert unique runs')
     parser.add_argument('-f', '--force', dest='force', action='store_true', help='allow to write to existing database file')
+
+
+def add_subcommand_db_potential(subparsers):
+    parser = subparsers.add_parser('potential', help='write evaluation potential to file')
+    parser.set_defaults(func=handle_subcommand_db_potential)
+    parser.add_argument('database', type=is_file_type, help='database to get potential from')
+    parser.add_argument('-e', '--evaluation', type=int, help='use evaluation id')
+    parser.add_argument('-o', '--output-filename', help='output potential filename', required=True)
 
 
 def handle_subcommand_db_merge(args):
@@ -31,3 +40,10 @@ def handle_subcommand_db_merge(args):
         src_dbm = DatabaseManager(input_database)
         print('merging:', input_database)
         copy_database_to_database(src_dbm, dest_dbm, only_unique=args.unique)
+
+
+def handle_subcommand_db_potential(args):
+    dbm = DatabaseManager(args.database)
+    if args.evaluation:
+        potential = select_potential_from_evaluation(dbm, args.evaluation)
+        potential.write_file(args.output_filename)
