41762aab617b81c8e74fa64bba4e6cd5df7c9ded
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-03-19 17:26:27 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-version='0.2.0'
+version='0.2.1'
 setup(
     name='dftfit',
     version=version,
