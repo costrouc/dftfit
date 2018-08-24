a23821eb4eafe164b8d5b65a5320f06fc3eb3474
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-08-23 22:56:31 -0400

    using mock to fix problem base for testing

--- tests/integration/test_dftfit_lammps_cython_algorithms.py
+++ tests/integration/test_dftfit_lammps_cython_algorithms.py
@@ -3,6 +3,8 @@
 """
 
 import pytest
+from unittest.mock import patch
+import random
 
 from dftfit.cli.utils import load_filename
 from dftfit.dftfit import dftfit
@@ -40,7 +42,33 @@ def test_lammps_cython_algorithms(algorithm):
         'population': 1 if 'nlopt' in algorithm else 8
     })
 
-    # Run optimization
-    run_id = dftfit(training_schema=training_schema,
-                    potential_schema=potential_schema,
-                    configuration_schema=configuration_schema)
+    # Run optimization with mocked python evaulation
+    class MockProblem:
+        def __init__(self, potential, training, features, *args, **kwargs):
+            self.potential = potential
+            self.features = features
+
+        def store_evaluation(self, potential, errors, value):
+            pass
+
+        def _fitness(self, paramters):
+            errors = tuple(np.random.random(len(self.features)).tolist())
+            value = random.random()
+            return errors, value
+
+        def __deepcopy__(self, memo):
+            return self # override copy method
+
+        def finalize(self):
+            pass
+
+        def __del__(self):
+            pass
+
+        def get_bounds(self):
+            return tuple(zip(*self.potential.optimization_bounds.tolist()))
+
+    with patch('dftfit.problem.DFTFITProblemBase', new=MockProblem):
+        run_id = dftfit(training_schema=training_schema,
+                        potential_schema=potential_schema,
+                        configuration_schema=configuration_schema)
