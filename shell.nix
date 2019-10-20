444760ff733964d09faa4dbf7e5e7557e14e3425
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-10-19 21:41:44 -0400

    Adding shell file

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python3Packages }:
+
+pkgs.mkShell {
+  buildInputs = with pythonPackages; [
+    # build dependecies
+    pkgs.lammps
+    pymatgen marshmallow pyyaml pygmo
+    pandas scipy numpy scikitlearn
+    lammps-cython pymatgen-lammps
+    # test dependencies
+    pytest pytest-benchmark pytestcov
+    # jupyter lab demonstrations
+  ];
+
+  shellHook = ''
+
+  '';
+}
